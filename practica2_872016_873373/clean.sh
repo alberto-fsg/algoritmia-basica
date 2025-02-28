a3ef64b1866d39ebf0ce8e583f5c53d04f6991cf
Alberto Francisco Solaz García <albertofranciscosolazgarcia@gmail.com>
2025-02-28 09:32:38 +0100

    Crear una carpeta de test por imagen en /tmp/pruebas/resultados

--- practica2_872016_873373/clean.sh
+++ practica2_872016_873373/clean.sh
@@ -4,7 +4,6 @@ rm -r ./libs/include
 rm -r ./libs/lib
 rm -r ./libs/share
 rm -r ./libs/lib64
-rm ./pruebas/resultados/*
 cd ./libs/dependencias/libjpeg
 rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile
 
--- practica2_872016_873373/ejecutar.sh
+++ practica2_872016_873373/ejecutar.sh
@@ -8,8 +8,19 @@ fi
 DIR_PRUEBAS='pruebas'
 OUTPUT_DIR="/tmp/${DIR_PRUEBAS}/resultados"
 
-# Crear directorio de salida si no existe
+# Crear directorio de salida principal si no existe
 mkdir -p "$OUTPUT_DIR"
 
-# Ejecutar el comando
-./costuras "${DIR_PRUEBAS}/profile.jpg" 150 "$OUTPUT_DIR"
+# Process profile.jpg
+profile_width=$(identify -format "%w" "${DIR_PRUEBAS}/profile.jpg")
+profile_arg=$((profile_width - 1))
+OUTPUT_PROFILE="${OUTPUT_DIR}/profile"
+mkdir -p "$OUTPUT_PROFILE"
+./costuras "${DIR_PRUEBAS}/profile.jpg" "$profile_arg" "$OUTPUT_PROFILE"
+
+# Process ejemplo-enunciado.jpg
+ejemplo_width=$(identify -format "%w" "${DIR_PRUEBAS}/ejemplo-enunciado.jpg")
+ejemplo_arg=$((ejemplo_width - 1))
+OUTPUT_EJEMPLO="${OUTPUT_DIR}/ejemplo-enunciado"
+mkdir -p "$OUTPUT_EJEMPLO"
+./costuras "${DIR_PRUEBAS}/ejemplo-enunciado.jpg" "$ejemplo_arg" "$OUTPUT_EJEMPLO"
