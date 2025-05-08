7f0652e85237d97ae06d3294da2f95d2658de593
Jorge872016 <872016@unizar.es>
2025-05-08 10:34:51 +0200

    Ejecutar .sh

--- practica4/ejecutar.sh
+++ practica4/ejecutar.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Para poder ejecutar en lab000
+rm -rf env
+/usr/bin/python3 -m venv --without-pip env
+source env/bin/activate
+curl https://bootstrap.pypa.io/pip/3.6/get-pip.py -o get-pip.py; python get-pip.py
+pip install pulp
+
+# Compilar
 echo "Compiling sources..."
 ./build.sh || exit 1
 
+# Ejecutar
 echo "Executing tests..."
 bin/transporte tests/pruebas.txt tests/resultados.txt
 python src/transporte.py tests/pruebas.txt tests/resultados_lineal_programming.txt
